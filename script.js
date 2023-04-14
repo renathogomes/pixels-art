2d196765fe8a7ee9d31fbea7d9983aeb94ce3027
renathogomes <renatho.20gomes@gmail.com>
2023-04-14 15:20:07 -0300

    zerado eu nao fico

---
+++ script.js
@@ -0,0 +1,6 @@
+const cabecalho = document.querySelector('#cabecalho');
+const tituloH1 = document.createElement('h1');
+cabecalho.appendChild(tituloH1);
+
+tituloH1.innerText = 'Paleta de Cores';
+tituloH1.id = 'title';
