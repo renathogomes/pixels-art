78fb7e349674ea24a21cef94860faf56946b2d06
renathogomes <renatho.20gomes@gmail.com>
2023-04-18 23:44:08 -0300

    finalmente o localStorage saiu

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-// Primeira - titulo h1 com id adicionados
-const cabecalho = document.querySelector('#cabecalho');
-const tituloH1 = document.createElement('h1');
-cabecalho.append(tituloH1);
 
-tituloH1.innerText = 'Paleta de Cores';
-tituloH1.id = 'title';
 
-// Criando paleta de cores
-const principal = document.querySelector('#principal');
-const paletaDeCores = document.createElement('section');
-principal.appendChild(paletaDeCores);
-paletaDeCores.id = 'color-palette';
-paletaDeCores.style.display = 'flex';
+const criaCabecalho = () => {
+    const cabecalho = document.querySelector('#cabecalho');
+    const tituloH1 = document.createElement('h1');
+    cabecalho.append(tituloH1);
+
+    tituloH1.innerText = 'Paleta de Cores';
+    tituloH1.id = 'title';
+}
+
+const criaSection = () => {
+    const principal = document.querySelector('#principal');
+    const paletaDeCores = document.createElement('section');
+    principal.appendChild(paletaDeCores);
+    paletaDeCores.id = 'color-palette';
+    paletaDeCores.style.display = 'flex';
+}
 
 const criaPaleta = (parametro) => {
     for (let index = 0; index < parametro; index += 1) {
@@ -27,7 +31,6 @@ const criaPaleta = (parametro) => {
     const primeiraCorPreta = document.querySelectorAll('.color')[0];
     primeiraCorPreta.style.background = 'black'
 }
-criaPaleta(4)
 
 const geraCores = () => {
     let r = Math.random() * 255;
@@ -38,28 +41,88 @@ const geraCores = () => {
 }
 
 const adicionaCores = () => {
+    let coresEmArray = [];
     let itensPintados = document.querySelectorAll('.color');
     for (let index = 1; index < itensPintados.length; index += 1) {
-        itensPintados[index].style.backgroundColor = geraCores()
+        itensPintados[index].style.backgroundColor = geraCores();
+        coresEmArray.push(itensPintados[index].style.backgroundColor = geraCores());
     }
-
 }
-onload = adicionaCores();
 
 const verificaCoresRepetidas = () => {
-    if ( document.querySelector('.color') === document.querySelector('.color').nextSibling.style.backgroundColor) {
+    if (document.querySelector('.color') === document.querySelector('.color').nextSibling.style.backgroundColor) {
         document.querySelector('.color').nextSibling.style.backgroundColor = geraCores()
     }
 }
+const salvaPeloAmor = () => {
+    // quem eu vou salvar?
+    const vouSalvarEsse = document.querySelector('#color-palette');
+    localStorage.setItem('CoresSalvas', vouSalvarEsse.innerHTML)
+}
+
+
+const criaBotão = () => {
+    const barraDeNavegacao = document.createElement('nav');
+    const botaoAdicionaCores = document.createElement('button');
+    botaoAdicionaCores.id = 'button-random-color';
+    barraDeNavegacao.appendChild(botaoAdicionaCores);
+    principal.appendChild(barraDeNavegacao);
+
+    botaoAdicionaCores.innerText = 'Cores aleatórias';
+    botaoAdicionaCores.addEventListener('click', () => {
+    adicionaCores();
+    salvaPeloAmor();
+    })
+
+    
+}
+
+const criaTabela = (parametro) => {
+    let quadroPixels = document.createElement('section');
+    quadroPixels.id = 'pixel-board';
+    const pai = document.querySelector('#principal');
+    pai.appendChild(quadroPixels);
+    for (let index = 0; index < parametro; index += 1) {
+        const pixelsLinha = document.createElement('div');
+        quadroPixels.appendChild(pixelsLinha);
+        pixelsLinha.className = 'pixel-div';
+
+    }
+}
+const criaColunas = (parametro) => {
+    for (let i = 0; i < parametro; i += 1) {
+        const paiDasColunas = document.getElementById('pixel-div')
+        const colunas = document.createElement('div');
+        paiDasColunas.append(colunas);
+
+        colunas.className = 'pixels';
+        colunas.style.border = '1px solid black';
+        colunas.style.width = '40px';
+        colunas.style.height = '40px';
+    }
+}
+
+const chamaStorage = () => {
+   const chama =  localStorage.getItem('CoresSalvas');
+   const pai = document.querySelector('#color-palette');
+
+   pai.innerHTML = chama;
+}
+
+
+const executaFuncioes = () => {
+    criaCabecalho();
+criaSection();
+criaBotão();
+criaPaleta(4);
+adicionaCores();
 verificaCoresRepetidas();
+criaTabela(5);
+}
 
-const barraDeNavegacao = document.createElement('nav');
-const botaoAdicionaCores = document.createElement('button');
-botaoAdicionaCores.id = 'button-random-color';
-barraDeNavegacao.appendChild(botaoAdicionaCores);
-principal.appendChild(barraDeNavegacao);
+executaFuncioes();
 
-botaoAdicionaCores.innerText = 'Cores aleatórias';
-
-botaoAdicionaCores.addEventListener('click', adicionaCores);
+window.onload = () => {
+    chamaStorage();
+}
 
